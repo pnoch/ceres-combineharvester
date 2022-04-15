7e1353c3c873735ef0324b6d26f040d72e768941
pnoch <p_noch@msn.com>
2022-04-15 18:03:54 +0700

    Update server.py

--- ceres/server/server.py
+++ ceres/server/server.py
@@ -35,7 +35,7 @@ def ssl_context_for_server(
     private_cert_path: Path,
     private_key_path: Path,
     *,
-    check_permissions: bool = False,
+    check_permissions: bool = True,
     log: Optional[logging.Logger] = None,
 ) -> Optional[ssl.SSLContext]:
     if check_permissions:
@@ -49,7 +49,7 @@ def ssl_context_for_server(
 
 
 def ssl_context_for_root(
-    ca_cert_file: str, *, check_permissions: bool = False, log: Optional[logging.Logger] = None
+    ca_cert_file: str, *, check_permissions: bool = True, log: Optional[logging.Logger] = None
 ) -> Optional[ssl.SSLContext]:
     if check_permissions:
         verify_ssl_certs_and_keys([Path(ca_cert_file)], [], log)
@@ -64,7 +64,7 @@ def ssl_context_for_client(
     private_cert_path: Path,
     private_key_path: Path,
     *,
-    check_permissions: bool = False,
+    check_permissions: bool = True,
     log: Optional[logging.Logger] = None,
 ) -> Optional[ssl.SSLContext]:
     if check_permissions:
